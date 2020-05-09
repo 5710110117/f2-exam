7fc2bfd37d0bb5e18ebc2eeb64a83a758ade0d11
5710110117 <5710110117@psu.ac.th>
2020-05-09 12:16:57 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
+import java.util.*;
 public class Main {
     public static void main(String agr[]){
-        double factor;
-        for(Sprite s : sprites){
-            if(s instanceof Zoomable){
-                Zoomable z = (Zoomable)s;
-                z.zoom(factor);
-            }
-        }
+        ArrayList<Shape> = mew ArrayList<Shape>();
+        Circle a = new Cirecle("G11",3.0);
+        shape.add(a);
+        shape.add(new Rectangle("G12",2.0,8.0));
+        shape.add(new Shpere("C",3.0));
+        shape.add(new Cylinder("D",3.0,2.0));
+        shape.add(new Cuboid("E",2.0,8.0,3.0));
+
+       
     }
     
 }
