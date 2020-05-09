950726862971301c455802a84a1782167276ec7c
5710110117 <5710110117@psu.ac.th>
2020-05-09 12:09:05 +0700

    Update Zoomable.java

--- Zoomable.java
+++ Zoomable.java
@@ -1,9 +1,7 @@
-public static void zoomAll(ArrayList<Sprite> sprites, double factor){
-    for(Sprite s : sprites){
-        if(s instanceof Zoomable){
-            Zoomable z = (Zoomable)s;
-            z.zoom(factor);
-        }
-    }
+public interface Zoomable {
+    void zoom(double factor);
+    
 
+
+    
 }
