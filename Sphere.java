403f1618a2a10fb085a8a0c65b70caff355c95d7
5710110117 <5710110117@psu.ac.th>
2020-05-09 12:40:53 +0700

    Update Sphere.java

--- Sphere.java
+++ Sphere.java
@@ -1,3 +1,11 @@
-public class Sphere implements AreaCalculable
-    
+public class Sphere extends Shape implements AreaCalculable, Zoomable,VolumeCalculable{
+    public double radius;
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+    public double getArea(){
+
+    }
+    public double getVolum
 }
