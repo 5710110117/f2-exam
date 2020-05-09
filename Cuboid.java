d95e095c9b607bbf84853707f1ba94760f719470
5710110117 <5710110117@psu.ac.th>
2020-05-09 12:40:25 +0700

    Update Cuboid.java

--- Cuboid.java
+++ Cuboid.java
@@ -1,19 +1,9 @@
-public class Cuboid extends Shape implements AreaCalculable {
+import java.util.Scanner;
+public class Cuboid extends Shape implements AreaCalculable, Zoomable{
     public String val;
     public Cuboid(String name){
         super(name);
 
     }
-    public String splitString2(String key){
-        key = name;
-        Scanner s = new Scanner(new File("data.txt"));
-        while(s.hashNextLine()){
-            String line =s.nextLine();
-            String item = line.split("_");
-            if(item[0].equals(key)){
-                val = item[1];
-               return val;
-            }
-        }
-    }
+    
 }
