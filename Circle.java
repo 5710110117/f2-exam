b29e1fb39cb37b7cc5ea3d79c39a2b6bdebb5413
5710110117 <5710110117@psu.ac.th>
2020-05-09 12:14:00 +0700

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -6,7 +6,17 @@ public class Circle extends Shape implements AreaCalculable {
         this.radius = radius;
     }
     public int splitString(String key){
-
+        int total = 0;
+        int r;
+        Scanner s = new Scanner(new File("data.txt"));
+        while(s.hashNextLine()){
+            String line =s.nextLine();
+            String item = line.split("_");
+            if(item[0].equals(key)){
+                r = Integer.parseInt(item[1]);
+                return r;
+            }
+        }
     }
     
     public void setRadius(String radius){
