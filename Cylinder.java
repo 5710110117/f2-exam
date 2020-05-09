fc892e059a634abef2a2b2eba9040c5781118660
5710110117 <5710110117@psu.ac.th>
2020-05-09 12:08:37 +0700

    Update Cylinder.java

--- Cylinder.java
+++ Cylinder.java
@@ -1,3 +1,22 @@
-public class Cylinder implements AreaCalculable, Zoomable {
+public class Cylinder extends Shape implements AreaCalculable,VolumeCalculable{
+    public double radius;
+    public double height;
+    public Cylinder(String name,double radius,double height)
+    {
+        super(name);
+        this.radius = radius;
+        this.height = height;
+    }
+
+    public double getArea(){
+		return Math.PI * radius * radius * height;
+    }
     
+    public double getVolume(){
+		return 2 * Math.PI * radius * height + 2 * Math.PI * radius * radius;
+    }
+    public void zoom(double factor)
+    {
+        radius = radius*factor;
+    }
 }
