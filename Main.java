fc6583ad5755e480985e8ce5e0bc13920396a6e9
5710110117 <5710110117@psu.ac.th>
2020-05-09 12:40:29 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String agr[]){
-        
+        double factor;
+        for(Sprite s : sprites){
+            if(s instanceof Zoomable){
+                Zoomable z = (Zoomable)s;
+                z.zoom(factor);
+            }
+        }
     }
     
 }
