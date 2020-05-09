2da83888880ee2ce30c37389a00ed06d904af566
5710110117 <5710110117@psu.ac.th>
2020-05-09 12:40:21 +0700

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,33 +1,31 @@
-public class Circle extends Shape implements AreaCalculable {
-    public double r;
-    public String radius;
-    public Circle(double radius){
+public class Circle extends Shape implements AreaCalculable, Zoomable
+{
+    public double radius;
+  
+
+    public Circle(String name,double radius)
+    {
         super(name);
         this.radius = radius;
     }
-    public int splitString(String key){
-        int total = 0;
-        int r;
-        Scanner s = new Scanner(new File("data.txt"));
-        while(s.hashNextLine()){
-            String line =s.nextLine();
-            String item = line.split("_");
-            if(item[0].equals(key)){
-                r = Integer.parseInt(item[1]);
-                return r;
-            }
-        }
-    }
-    
-    public void setRadius(String radius){
+
+    public void setRadius(double radius)
+    {
         this.radius = radius;
     }
-    public double getRadius(){
+
+    public double getRadius()
+    {
         return radius;
     }
-    
 
-    public double getArea(){
+    public double getArea()
+    {
         return Math.PI*radius*radius;
+    }
+
+    public void zoom(double factor)
+    {
+        radius = radius*factor;
     }
 }
