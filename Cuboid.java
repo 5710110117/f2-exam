42d1dde12bf4679749f0adddadb33aa7986303c4
5710110117 <5710110117@psu.ac.th>
2020-05-09 12:40:01 +0700

    Update Cuboid.java

--- Cuboid.java
+++ Cuboid.java
@@ -1,3 +1,19 @@
-public class Cuboid implements AreaCalculable {
-    
+public class Cuboid extends Shape implements AreaCalculable {
+    public String val;
+    public Cuboid(String name){
+        super(name);
+
+    }
+    public String splitString2(String key){
+        key = name;
+        Scanner s = new Scanner(new File("data.txt"));
+        while(s.hashNextLine()){
+            String line =s.nextLine();
+            String item = line.split("_");
+            if(item[0].equals(key)){
+                val = item[1];
+               return val;
+            }
+        }
+    }
 }
